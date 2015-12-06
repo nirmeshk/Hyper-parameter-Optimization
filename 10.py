3c7a10765a9a6ca27f32673a3ca242fb16715d2c
nirmesh <nirmesh.khandelwal@gmail.com>
2015-12-06 04:20:06 -0500

    memoization in GA_MODEL

--- 10.py
+++ 10.py
@@ -17,12 +17,16 @@ def cal_loss(baseline_population, population, model):
 
 if __name__ == '__main__':   
     for model in [GA_MODEL]:
-        dtlz1 = DTLZ_1(n=10, m=3)
+        dtlz1 = DTLZ_1(n=5, m=2)
         model = model(dtlz1)
         for optimizer in [de]:
             baseline_population, population = optimizer().optimize(model)
+            population = [x for x in population if x.objs_score is not None]
             print('Final population : ')
             for can in population: 
                 print(can.decs)
 
         print ("\n\n")
+
+    # untuned_baseline_population, untuned_population = ga.optimize(dtlz1)
+    # population.append()
