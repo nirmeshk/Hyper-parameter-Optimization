81c8e024735d4b2ee04ba407a9e0211a72c311f1
Nirmesh <nirmeshk@users.noreply.github.com>
2015-12-06 02:39:16 -0500

    Update 9.py

--- 9.py
+++ 9.py
@@ -7,21 +7,16 @@ import logging
 if __name__ == '__main__':
     # DTLZ 1,3,5,7 with 2,4,6,8 objectives and 10,20,40 decisions
     
-    #for obj in [2, 4, 6, 8]
+    for obj in [2, 4, 6, 8]:
     #for dec in [10, 20, 40]
+        for model in [DTLZ_1, DTLZ_3, DTLZ_5, DTLZ_7]:
+            for optimizer in [ga]:
+                with open('out/' + model.__name__ + '_' + str(obj) + '_' + str(40), 'w+') as fp:
+                    model_instance =  model(m = obj, n = 40)    
+                    baseline_population, population = optimizer(fp=fp).optimize(model_instance)      
+                    c = divergence_from_baseline(baseline_population, population, model_instance)
+                    log_base_and_final(baseline_population, population, model_instance, fp)
+                    fp.write("divergence_from_baseline:{}".format(c))
+                    fp.write('\n')
 
-    for model in [Kursawe]:#DTLZ_1, DTLZ_3, DTLZ_5, DTLZ_7]:
-        for optimizer in [ga]:
-            model_instance =  model()    
-            baseline_population, population = optimizer().optimize(model_instance)
-            print(model_instance.energy(population[0]))
-
-            #en = cal_loss(baseline_population, population, model_instance)
-            #if optimizer.__name__ == 'ga':
-            #    k = 0
-            #else:
-            #    k = 1
-            #rdiv[k].extend(en)
-        #print([rdiv])
-        #rdivDemo(rdiv)
-        #print ("\n\n")
+    
